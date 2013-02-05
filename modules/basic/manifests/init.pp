cabf50d61375ab87f7d755425a7c85145db0af38
Deepak <deepak@citrusinformatics.com>
2013-02-05 20:20:10 +0530

    6

--- modules/basic/manifests/init.pp
+++ modules/basic/manifests/init.pp
@@ -46,27 +46,72 @@ class tiger::install {
 					 }
 
 class psad::install {
-	    package { 'psad':
-		ensure => present,
-	            }
-					}
 
+$addr =  'deepak@citrusinformatics.com;'
+$strng = 'EMAIL_ADDRESSES'
+$cfile = '/etc/psad/psad.conf'
+
+    package { 'psad':
+                ensure => present,
+            }
+    service { "psad":
+            ensure    => "running",
+            enable    => "true",
+            require   => Package["psad"],
+        hasrestart => true,
+        hasstatus => true,
+        subscribe => File["psad.conf"],
+			}
+
+    file    {  "psad.conf":
+
+             mode    => 644,
+             owner   => "root",
+             group   => "root",
+             ensure    => present,
+             require => Package["psad"],
+             path    => "/etc/psad/psad.conf",
+            }
+
+    exec    {"sed -i '/${strng}/c\EMAIL_ADDRESSES \t ${addr}' '${cfile}'" :
+             require   =>  Package["psad"],
+              notify   =>  Service["psad"],
+            }
+                     }   
 class nmap::install {
-package { 'nmap':
+		package { 'nmap':
 		ensure => present,
-        }
+				}
 					}
+					
 class logwatch::install {
-
-package { 'logwatch':
+		package { 'logwatch':
 		ensure => present,
-        }
+					}
 						}
+						
+class fail2ban::install {
 		
+		package { 'fail2ban':
+		ensure => present,
+				}						
+						}
+
+class whoopsie::remove  { 
+package { 'whoopsie':
+    ensure => purged,
+		}						
+						}		
+
+
 class basic {
              include etckeeper::install
              include tiger::install
 			 include psad::install
              include nmap::install
              include logwatch::install 			 
+			 include fail2ban::install
+			 include whoopsie::remove 
+			 
 			}
+
