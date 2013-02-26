1f281c567701dc9a5c16a8a0ad3f0c7e85c48a79
Deepak <deepak@citrusinformatics.com>
2013-02-26 16:02:32 +0530

    16

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -2,6 +2,7 @@ class preq::install  {
  
 exec { "preq1" :
         command => "gpg --keyserver  hkp://keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A",
+     before => File ["/etc/apt/sources.list"], 
      }
 	 
 exec { "preq2" :
@@ -14,12 +15,12 @@ $filename = '/etc/apt/sources.list'
 
 exec { "cmd1" :
        command => "echo deb http://repo.percona.com/apt precise main >> '${filename}'",
-       require => File ["/etc/apt/sources.list"],
+      require => Exec ['preq2'],
      }
 
 exec { "cmd2" :
        command => "echo deb-src http://repo.percona.com/apt precise main >> '${filename}'",
-       require => File ["/etc/apt/sources.list"],
+       require => Exec ['cmd1'],
      }
 					  }
  
@@ -31,6 +32,7 @@ include preq::install
 exec {"1":
 command => "/usr/bin/apt-get -y update",
 #require => Class["preq::install"],
+require => Exec["cmd2"], 
      }
 	 
 exec {"2":
